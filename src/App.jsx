3b506f5d3e63e758236c859c036badbb091eb1d1
Pinku <pinkuparmer96@gmail.com>
2024-09-30 17:12:20 +0530

    message

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Input from './components/Input/Input'
+import Output from './components/Output/Output'
+
+function App() {
+  const [timeTable, setTimeTable] = useState([]);
+
+  function setRoutine(routine) {
+    setTimeTable([...timeTable, routine]);
+  }
+
+
+  return (
+    <>
+      <div className="main">
+        <Input setRoutine={setRoutine} />
+        {timeTable.map((routine, index) => (
+          <Output key={index} hour={routine.hour} subject={routine.subject} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App
